ab8a6878f65f719b79d226ad4e08bcfa5843077e
FadyGerges23 <fady.gerges@espace.com.eg>
2023-11-16 12:38:03 +0200

    Added display_name field to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_15_093352) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_16_085047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_15_093352) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "jti", null: false
+    t.string "display_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
